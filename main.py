bbad046e27fd150d88bb57464e9090685c5e9eb0
Zach Clendenen <zclendenen@yahoo.com>
2025-01-26 13:57:25 +0000

    refactor

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
 
