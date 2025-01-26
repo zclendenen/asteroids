52d27005d624d7ef43176b59a4a8941549550b99
Zach Clendenen <zclendenen@yahoo.com>
2025-01-26 20:02:36 +0000

    added render for player

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     dt = 0
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
@@ -14,6 +18,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit to 60 FPS
