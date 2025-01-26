11c252390704e9a4d000d20b94e2f7b4aac2ddf7
Zach Clendenen <zclendenen@yahoo.com>
2025-01-26 14:11:00 -0600

    cleaned up import and consolodated variables into args

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from player import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
     dt = 0
-    player = Player(x, y)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
