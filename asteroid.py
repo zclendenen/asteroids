adf376ea922f5e7c77abdf60573c4c7b62c158be
Zach Clendenen <zclendenen@yahoo.com>
2025-01-26 19:10:15 -0600

    corrected init of asteroid to contain x, y, radius

--- asteroid.py
+++ asteroid.py
@@ -6,7 +6,7 @@ from constants import *
 class Asteroid(CircleShape):
 
     def __init__(self, x, y, radius):
-        super().__init__
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
