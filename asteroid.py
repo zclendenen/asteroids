0196f055696b88e30d1069d38571a383eb0e4ad2
Zach Clendenen <zclendenen@yahoo.com>
2025-01-27 06:20:38 +0000

    correct math in velocity calculation

--- asteroid.py
+++ asteroid.py
@@ -20,10 +20,13 @@ class Asteroid(CircleShape):
         if self.radius <= ASTEROID_MIN_RADIUS:
             return
         random_angle = random.uniform(20, 50)
+
         vector_1 = self.velocity.rotate(random_angle)
         vector_2 = self.velocity.rotate(-random_angle)
+
         new_radius = self.radius - ASTEROID_MIN_RADIUS
+
         asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
         asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
-        asteroid_1.velocity(vector_1) * 1.2
-        asteroid_2.velocity(vector_2) * 1.2
+        asteroid_1.velocity(vector_1 * 1.2)
+        asteroid_2.velocity(vector_2 * 1.2)
