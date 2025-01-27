82f032e1d6f30d36180470369cef10c4db3cee12
Zach Clendenen <zclendenen@yahoo.com>
2025-01-26 20:36:54 -0600

    corrected init and draw to reflect the correct variables

--- shot.py
+++ shot.py
@@ -4,11 +4,11 @@ from constants import *
 
 class Shot(CircleShape):
         
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, SHOT_RADIUS)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += (self.velocity * dt)
