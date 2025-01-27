248103abb0e96014395f5251c1c536f00fe1eaa0
Zach Clendenen <zclendenen@yahoo.com>
2025-01-27 06:18:11 +0000

    change asteroid to split instead of kill

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
             for shot in shots:
                 if asteroid.collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
 
         screen.fill("black")
